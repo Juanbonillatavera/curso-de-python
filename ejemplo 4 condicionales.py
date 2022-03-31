1cf5b033e8d6386ecf61c2402ea28ecf9051e7cd
Juanbonillatavera <101885071+Juanbonillatavera@users.noreply.github.com>
2022-03-31 11:46:48 -0500

    6666
    6666

--- ejemplo 4 condicionales.py
+++ ejemplo 4 condicionales.py
@@ -1,12 +1,13 @@
-from sys import flags
+print("elija una de las siguientes banderas a continuacion:")
+print ("carreras,roja,arcoiris y pirata")
 
+paises=input("digite la abndera seleccionada")
 
-pais= str(input("Digite el pais que necesite"))
-if pais=="italia":
-    banderas= ("U0001F3C1")
-    print("/U0001F3C1")
+if paises == 'carreras':
+   banderas= ("U0001F3C1")
+print("/U0001F3C1")
     
-else :
-    print ("nada")
+elif paises == 'roja':    
+print ("nada")
     
 
