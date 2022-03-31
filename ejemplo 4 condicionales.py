1256641cccd650ccd4940371634aed48cb0c50c2
Juanbonillatavera <101885071+Juanbonillatavera@users.noreply.github.com>
2022-03-31 10:18:05 -0500

    por corregir

---
+++ ejemplo 4 condicionales.py
@@ -0,0 +1,12 @@
+from sys import flags
+
+
+pais= str(input("Digite el pais que necesite"))
+if pais=="italia":
+    banderas= ("U0001F3C1")
+    print("/U0001F3C1")
+    
+else :
+    print ("nada")
+    
+
