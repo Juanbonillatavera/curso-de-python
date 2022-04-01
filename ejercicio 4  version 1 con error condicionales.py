c2228a1afe27f7239458f700014d82d00a0970cb
Juanbonillatavera <101885071+Juanbonillatavera@users.noreply.github.com>
2022-04-01 17:17:43 -0500

    ejercicios faltantes

---
+++ ejercicio 4  version 1 con error condicionales.py
@@ -0,0 +1,27 @@
+print("este es un programa que simulara la accion del sistema ubuntus linux")
+print("introduciremos los siguiente valores en este orden tecla1 sera ctrl tecla 2 sera alt tecla 3 del")
+ 
+tecla1= str (input("introduzca tecla 1"))
+tecla2= str (input("introduzca tecla 2"))
+tecla3= str (input("introduzca tecla 3"))
+
+     
+if  tecla1=="ctrl":
+    if tecla2 == "alt":
+        print("falta tecla 3")
+    else:                
+        print("comando erroneo")
+    if  tecla3 == "del":
+        print("log out")
+    else:                
+        print("comando erroneo")
+            
+
+   
+
+
+
+ 
+ 
+ 
+ 
