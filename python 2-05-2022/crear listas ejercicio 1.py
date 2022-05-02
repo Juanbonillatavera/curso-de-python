c1467e4034f88f8401665008688f4b53b82c31ac
Juanbonillatavera <101885071+Juanbonillatavera@users.noreply.github.com>
2022-05-02 11:52:48 -0500

    trabajo incompleto de hoy

---
+++ python 2-05-2022/crear listas ejercicio 1.py
@@ -0,0 +1,10 @@
+print ('la lista a la que se le sacara la raiz cuadrada1,2,3,4,5')
+
+import math
+
+lista=[1,2,3,4,5]
+a=[]
+
+for i in lista:
+    a.append(math.sqrt(i))
+print('las raices cuadres de la lista son',a)
---
+++ python 2-05-2022/ejercicio 2corregido.py
@@ -0,0 +1,16 @@
+from cmath import sqrt
+
+
+lista=[]
+limite=(int(input('ingrese el numero que sera el limite de la lista')))
+
+for x in range(limite):
+    valor=int(input("Ingrese un valor entero:"))
+    lista.append(valor**3)
+print(lista)
+
+            
+    
+ 
+    
+  
