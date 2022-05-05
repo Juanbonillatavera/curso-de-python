399b5a56158fd05d9457d0c9f80071e559750ecc
Juanbonillatavera <101885071+Juanbonillatavera@users.noreply.github.com>
2022-05-05 10:39:28 -0500

    .

---
+++ python 2-05-2022/ejercicio 6.py
@@ -0,0 +1,7 @@
+a=[]
+print('este programa sirve para sacar el cuadrado de un numero')
+limite=int(input('escriba el numero limite de su lista '))
+for i in range (limite):
+    valores=int(input('escriba los valores: '))
+    a.append(valores**2)
+    print(a)
