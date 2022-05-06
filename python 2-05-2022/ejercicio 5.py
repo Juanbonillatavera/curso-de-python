1050e86389f85e4eb53d0e24b0ffba994762717b
Juanbonillatavera <101885071+Juanbonillatavera@users.noreply.github.com>
2022-05-06 06:45:39 -0500

    Create ejercicio 5.py

---
+++ python 2-05-2022/ejercicio 5.py
@@ -0,0 +1,13 @@
+
+frase=input('digite la frase: ')
+
+cuentafrase=frase.split()
+
+cuentafrase=list(map(str.upper,cuentafrase))
+
+print(cuentafrase)
+
+sinduplicado=set(cuentafrase)
+
+print(sinduplicado)
+
