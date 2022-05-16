27bfff05797ba9da8e5a150973b0cde479a5b69c
Juanbonillatavera <101885071+Juanbonillatavera@users.noreply.github.com>
2022-05-16 09:55:17 -0500

    ejercicio propuesto instructora diccionarios y ejercicio 5 del 9-05-2022

---
+++ python 9-05-2022/ejercicio 5.py
@@ -0,0 +1,8 @@
+
+edad1=int(input('ingrese la primera edad '))
+edad2=int(input('ingrese la segunda edad '))
+edad3=int(input('ingrese la tercera edad '))
+
+promedio= (edad1+edad2+edad3)/3
+
+print('el promedio es: ', promedio)
